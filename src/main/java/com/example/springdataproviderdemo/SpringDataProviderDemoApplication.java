fb829914b96594057f361ddfab0f70ca37d121b4
envieol <371755714@qq.com>
2020-05-31 16:50:33 +0800

    initialize

---
+++ src/main/java/com/example/springdataproviderdemo/SpringDataProviderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataproviderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataProviderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataProviderDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdataproviderdemo/SpringDataProviderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataproviderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataProviderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
